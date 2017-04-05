33f3186fb6ef687964bd46532682e2737e9b0e9b
hflint <hmcmflint@gmail.com>
2017-04-04 19:53:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeNameOnLoad() {
+	var nameSpot = document.getElementById('name');
+
+	var	userName = prompt("What is your name?");
+
+	nameSpot.innerHTML = ", " +userName; 
+}
+changeNameOnLoad();
